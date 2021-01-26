e6a14f1c0b0b74b3be407f700c33b86e33bffc5e
Serg Kotovsky <info@geekserg.ru>
2021-01-26 18:16:15 +0300

    исправление правка пути для импорта пакета sorter

--- 04_sort/main.go
+++ 04_sort/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/seggga/golang1/sorter"
+
+	"github.com/seggga/golang1/04_sort/sorter"
 )
 
 func main() {
 
 	// неотсортированный слайс
-	userSlice := []int{9,8,7,6,5,4,3,2,1}
+	userSlice := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
 	fmt.Printf("%#v", userSlice)
 
 	// сортировка слайса
