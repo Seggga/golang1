6d6385b86e65e6094d710adc18050ec5d803033c
Serg Kotovsky <info@geekserg.ru>
2021-02-16 00:19:34 +0300

    добавил  JSON и YAML. Валидация URL в отдельной функции.

--- 08_configreader/conf/conf.go
+++ 08_configreader/conf/conf.go
@@ -1,15 +1,21 @@
 package conf
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
-//ConfigType - is a structure with configuration elements user application required
-type ConfigType struct {
+//ConfigStruct - is a structure with configuration elements user application required
+type ConfigStruct struct {
 	Port        uint32
 	DBURL       string
 	JaegerURL   string
@@ -19,8 +25,30 @@ type ConfigType struct {
 	SomeAppKey  string
 }
 
+//ConfigJSON - is a structure with annotation for JSON unmarshaling
+type ConfigJSON struct {
+	Port        uint32 `json:"port"`
+	DBURL       string `json:"db_url"`
+	JaegerURL   string `json:"jaeger_url"`
+	SentryURL   string `json:"sentry_url"`
+	KafkaBroker string `json:"kafka_broker"`
+	SomeAppID   string `json:"some_app_id"`
+	SomeAppKey  string `json:"some_app_key"`
+}
+
+//ConfigYAML - is a structure with annotation for YAML unmarshalling
+type ConfigYAML struct {
+	Port        uint32 `yaml:"port"`
+	DBURL       string `yaml:"db_url"`
+	JaegerURL   string `yaml:"jaeger_url"`
+	SentryURL   string `yaml:"sentry_url"`
+	KafkaBroker string `yaml:"kafka_broker"`
+	SomeAppID   string `yaml:"some_app_id"`
+	SomeAppKey  string `yaml:"some_app_key"`
+}
+
 //ReadFlags reads config data from command-line flags and writes it back in the ConfigType structure
-func ReadFlags(config *ConfigType) error {
+func ReadFlags(config *ConfigStruct) error {
 
 	// a set of flags used by the "flag"-package
 	var (
@@ -28,7 +56,7 @@ func ReadFlags(config *ConfigType) error {
 		dbURL       = flag.String("db_url", "", "Database URL (postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable).")
 		jaegerURL   = flag.String("jaeger_url", "", "Jaeger URL with port number (http://example:9000).")
 		sentryURL   = flag.String("sentry_url", "", "Sentry URL with port number(http://sentry:9000).")
-		kafkaBroker = flag.String("kafka_broker", "", "Kafka broker URL with port number (kafka:9092).")
+		kafkaBroker = flag.String("kafka_broker", "", "Kafka broker URL with port number (ssl://kafka:9092).")
 		someAppID   = flag.String("some_app_id", "", "Unique application identifier (testid).")
 		someAppKey  = flag.String("some_app_key", "", "Test key for the application (testkey).")
 	)
@@ -38,31 +66,34 @@ func ReadFlags(config *ConfigType) error {
 	// obtain PORT data
 	config.Port = uint32(*port)
 
-	// data validation for "db_url" flag
-	if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, *dbURL); !matched && *dbURL != "" {
-		err := fmt.Errorf("Not valid data for the flag db_url, the field should be a URL-string, you entered %v", *dbURL)
-		return err
+	// data validation 1 of 4
+	// check, if db_url is valid, mandatory field
+	if *dbURL == "" {
+		return fmt.Errorf("error processing db_url, the field is mandatory")
+	}
+	if err := isURL(*dbURL); err != nil {
+		return fmt.Errorf("error processing db_url, %v", err)
 	}
 	config.DBURL = *dbURL
 
-	// data validation for "jaeger_url" flag
-	if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, *jaegerURL); !matched && *jaegerURL != "" {
-		err := fmt.Errorf("Not valid data for the flag jaeger_url, the field should be a URL-string with port")
-		return err
+	// data validation 2 of 4
+	// check, if jaeger_url is valid
+	if err := isURL(*jaegerURL); err != nil && *jaegerURL != "" {
+		return fmt.Errorf("error processing jaeger_url, %v", err)
 	}
 	config.JaegerURL = *jaegerURL
 
+	// data validation 3 of 4
 	// data validation for "sentry_url" flag
-	if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, *sentryURL); !matched && *sentryURL != "" {
-		err := fmt.Errorf("Not valid data for the flag sentry_url, the field should be a URL-string with port")
-		return err
+	if err := isURL(*sentryURL); err != nil && *sentryURL != "" {
+		return fmt.Errorf("error processing sentry_url, %v", err)
 	}
 	config.SentryURL = *sentryURL
 
+	// data validation 4 of 4
 	// data validation for "kafka_broker" flag
-	if matched, _ := regexp.MatchString(`\w+:\d+`, *kafkaBroker); !matched && *kafkaBroker != "" {
-		err := fmt.Errorf("Not valid data for the flag kafka_broker, the field should be a URL-string with port")
-		return err
+	if err := isURL(*kafkaBroker); err != nil && *kafkaBroker != "" {
+		return fmt.Errorf("error processing kafka_broker, %v", err)
 	}
 	config.KafkaBroker = *kafkaBroker
 
@@ -73,64 +104,56 @@ func ReadFlags(config *ConfigType) error {
 }
 
 //ReadEnv reads data from Environment variables and fills up the ConfigType structure
-func ReadEnv(config *ConfigType) error {
+func ReadEnv(config *ConfigStruct) error {
 
+	// check, if PORT is valid, has default value 8000
 	envData := os.Getenv("PORT")
-
 	if envData == "" {
 		envData = "8000"
 	} else if matched, _ := regexp.MatchString(`^\d+`, envData); !matched {
-		err := fmt.Errorf("Not valid data for the PORT variable. Entered value is %s", envData)
+		err := fmt.Errorf("not valid data for the PORT variable. Entered value is %s", envData)
 		return err
 	}
 	number, err := strconv.Atoi(envData)
 	if err != nil {
-		err1 := fmt.Errorf("Error parsing port number %v", err)
-		return err1
+		return fmt.Errorf("error parsing port number %v", err)
 	}
 	config.Port = uint32(number)
 
-	// check mandatory fields
+	// data validation 1 of 4
+	// check, if db_url is valid, mandatory field
 	envData = os.Getenv("db_url")
 	if envData == "" {
-		err := fmt.Errorf("Database URL is not set")
-		return err
+		return fmt.Errorf("error processing db_url, the field is mandatory")
 	}
-	if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, envData); !matched {
-		err := fmt.Errorf("Variable db_url fail, expected URL-string, Entered value is %s", envData)
-		return err
+	if err := isURL(envData); err != nil {
+		return fmt.Errorf("error processing db_url, %v", err)
 	}
 	config.DBURL = envData
 
-	// data validation for "jaeger_url" variable
+	// data validation 2 of 4
+	// check, if jaeger_url is valid
 	envData = os.Getenv("jaeger_url")
-	if envData != "" {
-		if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, envData); !matched {
-			err := fmt.Errorf("Variable jaeger_url fail. Expected URL-string with port. Entered value is %s", envData)
-			return err
-		}
-		config.JaegerURL = envData
+	if err := isURL(envData); err != nil && envData != "" {
+		return fmt.Errorf("error processing jaeger_url, %v", err)
 	}
+	config.JaegerURL = envData
 
-	// data validation for "sentry_url" variable
+	// data validation 3 of 4
+	// check, if sentry_url is valid
 	envData = os.Getenv("sentry_url")
-	if envData != "" {
-		if matched, _ := regexp.MatchString(`\w+://\w+.?\w+\:`, envData); !matched {
-			err := fmt.Errorf("Variable sentry_url fail. Expected URL-string with port. Entered value is %s", envData)
-			return err
-		}
-		config.SentryURL = envData
+	if err := isURL(envData); err != nil && envData != "" {
+		return fmt.Errorf("error processing sentry_url, %v", err)
 	}
+	config.SentryURL = envData
 
-	// data validation for "kafka_broker" variable
+	// data validation 4 of 4
+	// check, if kafka_broker is valid
 	envData = os.Getenv("kafka_broker")
-	if envData != "" {
-		if matched, _ := regexp.MatchString(`\w+:\d+`, envData); !matched {
-			err := fmt.Errorf("Variable kafka_broker fail. Expected URL-string with port. Entered value is %s", envData)
-			return err
-		}
-		config.KafkaBroker = envData
+	if err := isURL(envData); err != nil && envData != "" {
+		return fmt.Errorf("error processing kafka_broker, %v", err)
 	}
+	config.KafkaBroker = envData
 
 	// obtain "some_app_id" data
 	envData = os.Getenv("some_app_id")
@@ -144,5 +167,104 @@ func ReadEnv(config *ConfigType) error {
 		config.SomeAppKey = envData
 	}
 
+	return nil
+}
+
+//ReadFile detects the filetype and decides, what function to call for config-parsing
+func ReadFile(config *ConfigStruct, filePath string) error {
+	//obtain "filepath" extension
+	dotSlice := strings.Split(strings.ToLower(filePath), ".")
+	fileExt := dotSlice[len(dotSlice)-1]
+	switch fileExt {
+	case "yml":
+		return readYAML(config, filePath)
+	case "yaml":
+		return readYAML(config, filePath)
+	case "json":
+		return readJSON(config, filePath)
+	default:
+		return fmt.Errorf("unknown file type, *.json / *.yml / *.yaml expexted, *.%s detected", fileExt)
+	}
+}
+
+//ReadYAML reads data config from YAML-file and fills up the ConfigStruct structure
+func readYAML(config *ConfigStruct, filePath string) error {
+	//file opening
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("cannot open file %s, %v", filePath, err)
+	}
+	//deffered file closing
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			panic(fmt.Sprintf("error while closing file %s, %v", filePath, err))
+		}
+	}()
+
+	configYAML := ConfigYAML{}
+	err = yaml.NewDecoder(file).Decode(&configYAML)
+	if err != nil {
+		log.Printf("cannot decode YAML file %s in a structure: %v", filePath, err)
+	}
+
+	config.Port = configYAML.Port
+	config.DBURL = configYAML.DBURL
+	config.JaegerURL = configYAML.JaegerURL
+	config.SentryURL = configYAML.SentryURL
+	config.KafkaBroker = configYAML.KafkaBroker
+	config.SomeAppID = configYAML.SomeAppID
+	config.SomeAppKey = configYAML.SomeAppKey
+
+	return nil
+}
+
+//ReadJSON reads data config from JSON-file and fills up the ConfigStruct structure
+func readJSON(config *ConfigStruct, filePath string) error {
+	//file opening
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("cannot open file %s, %v", filePath, err)
+	}
+	//deffered file closing
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			panic(fmt.Sprintf("error while closing file %s, %v", filePath, err))
+		}
+	}()
+
+	configJSON := ConfigJSON{}
+	err = json.NewDecoder(file).Decode(&configJSON)
+	if err != nil {
+		log.Printf("cannot decode JSON file %s in a structure: %v", filePath, err)
+	}
+
+	config.Port = configJSON.Port
+	config.DBURL = configJSON.DBURL
+	config.JaegerURL = configJSON.JaegerURL
+	config.SentryURL = configJSON.SentryURL
+	config.KafkaBroker = configJSON.KafkaBroker
+	config.SomeAppID = configJSON.SomeAppID
+	config.SomeAppKey = configJSON.SomeAppKey
+
+	return nil
+}
+
+//isURL implements URL validation
+func isURL(data string) error {
+
+	url, err := url.Parse(data)
+	if err != nil {
+		return err
+	}
+	// no protocol in db_url
+	if url.Scheme == "" {
+		return fmt.Errorf("No protocol has been set. Entered data is %v", data)
+	}
+	// no servername/address in db_url
+	if url.Host == "" {
+		return fmt.Errorf("No host has been set. Entered data is %v", data)
+	}
 	return nil
 }
--- 08_configreader/main.go
+++ 08_configreader/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/seggga/golang1/08_configreader/conf"
 )
 
 func main() {
-	var config conf.ConfigType
+	var config conf.ConfigStruct
 
 	//err := conf.ReadEnv(&config) // fill in the config-structure with OS variables
-	err := conf.ReadFlags(&config) // fill in the config-structure with flags-data
+	//err := conf.ReadFlags(&config) // fill in the config-structure with flags-data
+
+	confiigFile := os.Args[1]
+	err := conf.ReadFile(&config, confiigFile)
+
 	if err != nil {
 		fmt.Printf("%v", err)
 		return
